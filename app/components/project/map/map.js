9c9ae4ed104d68f55b8e1973ff3488134c5acd0c
aUngurian <a.ungurian@peppers-studio.ru>
2022-04-05 14:50:06 +0300

    chore(map): добавлены кнопка и анимация разрушения

--- app/components/project/map/map.js
+++ app/components/project/map/map.js
@@ -1,35 +1,33 @@
-const templateDiv = document.querySelector('.map__template');
+//переменные для врапперов карты
 const template = document.querySelector('#svg-template');
+const templateDiv = document.querySelector('.map__template');
 const completeDiv = document.querySelector('.map__complete');
+const tCoord = template.getBoundingClientRect();
 
-
-template.style.width = `${template.clientWidth*1}px`;
-template.style.height = `${template.clientHeight*1}px`;
 templateDiv.style.opacity = '1';
-
+//контейнерам размеры svg карты
 [templateDiv,completeDiv].forEach((el)=> {
     el.style.width = `${template.clientWidth}px`;
     el.style.height = `${template.clientHeight}px`;
 });
-// completeDiv.style.width = `${template.clientWidth}px`;
-// completeDiv.style.height = `${template.clientHeight}px`;
 
+//переменные для кусков
 const partsDiv = document.querySelectorAll('.map__part');
 const parts = document.querySelectorAll('img');
-
-const tCoord = template.getBoundingClientRect();
-
+//контейнеры кусков равны размеру внутри
 Array.from(parts).forEach((el,id)=> {
     let w = el.offsetWidth;
     let h = el.offsetHeight;
     let x = partsDiv[id].dataset.xxx;
     let y = partsDiv[id].dataset.yyy;
-    console.log("x",x)
-
-    el.style.width = `${w*1}px`;
-    el.style.height = `${h*1}px`;
     partsDiv[id].style.top = `${y}px`;
     partsDiv[id].style.left = `${x}px`;
+});
+
+document.addEventListener('click', function (event) {
+    const target = event.target.closest('.button');
+    if(target === null) return;
+    templateDiv.classList.add('map__template_crashed')
 });
 
 
@@ -37,18 +35,15 @@ document.addEventListener('mousedown', function (event) {
     const target = event.target.closest('.map__part');
     if(target === null) return;
 
-    console.log('target',target)
+    console.log('target',target);
     let shiftX = event.clientX - target.getBoundingClientRect().left;
     let shiftY = event.clientY - target.getBoundingClientRect().top;
     target.style.position = 'absolute';
     target.style.zIndex = '100';
 
-
     moveAt(event.pageX, event.pageY);
 
     function moveAt(pageX, pageY) {
-        // target.style.left = pageX + 'px';
-        // target.style.top = pageY + 'px';
         target.style.left = pageX - tCoord.left - shiftX + 'px';
         target.style.top = pageY - shiftY + 'px';
     }
