4941f73d7f91062741e7f63d0fa692232895e92a
anton <anton.ystu19@gmail.com>
2022-04-08 23:42:01 +0300

    feat(work): добавлена карусель фотографий

--- app/components/project/gui/carousel/carousel.js
+++ app/components/project/gui/carousel/carousel.js
@@ -16,7 +16,29 @@ $(() => {
         watchOverflow: true
       }
     }
-  
+  });
+
+  var swiper_2 = new Swiper('.swiper-container_2', {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: false,
+    spaceBetween: 40,
+    breakpoints: {
+      320: {
+
+      },
+      1024: {
+
+      }
+    },
+    pagination: {
+      el: '.work__photos-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.work__photos-nav_next',
+      prevEl: '.work__photos-nav_prev',
+    },
   });
 
 });
